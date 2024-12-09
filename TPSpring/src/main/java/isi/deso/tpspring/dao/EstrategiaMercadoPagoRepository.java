c4bd98101561866613d3cfc807036ac952efd726
Marcos <marcosjd2005@gmail.com>
2024-12-09 20:39:07 -0300

    Dao parte Marcos

---
+++ TPSpring/src/main/java/isi/deso/tpspring/dao/EstrategiaMercadoPagoRepository.java
@@ -0,0 +1,10 @@
+package isi.deso.tpspring.dao;
+
+import isi.deso.tpspring.model.EstrategiaMercadoPago;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstrategiaMercadoPagoRepository extends JpaRepository<EstrategiaMercadoPago, Integer> {
+
+}
---
+++ TPSpring/src/main/java/isi/deso/tpspring/dao/EstrategiaTransferenciaRepository.java
@@ -0,0 +1,10 @@
+package isi.deso.tpspring.dao;
+
+import isi.deso.tpspring.model.EstrategiaTransferencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstrategiaTransferenciaRepository extends JpaRepository<EstrategiaTransferencia, Integer> {
+
+}
